df4d39f7863dd7e7f956db7d8251bfc58f743fa6
Noushid Pakath <noushid.pakath@airpay.co.in>
2022-06-10 17:00:51 +0530

    add some usuful instructions and minor changes made

--- README.md
+++ README.md
@@ -8,4 +8,15 @@ Steps to run
     - npm install
     - npm run dev
 - create database and config in .env
+Run Following
+- php artisan migrate --seed
+- php artisan key:generate
 - run php artisan ser
+
+Click on login using below credential
+
+username - admin@admin.com
+password - password
+
+Note: please run below command if Images are note visble in order making window.
+php artisan storage:link
--- resources/views/createCategory.blade.php
+++ resources/views/createCategory.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Products') }}</div>
+                    <div class="card-header">{{ __('Create Category') }}</div>
                     <div class="card-body">
                         @if ($errors->any())
                             <div class="alert alert-danger">
